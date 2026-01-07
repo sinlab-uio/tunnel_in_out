f0707649156f012a81a9b673ce8b622d4e029eb5
Carsten Griwodz <griff@ifi.uio.no>
2026-01-07 10:11:46 +0100

    changing outside ports to WebRTC standard traversal ports

--- test/live-tunnel-server.sh
+++ test/live-tunnel-server.sh
@@ -4,11 +4,15 @@ export PATH=$PATH:/home/griff/Install/tunnel/bin
 
 echo "================================================================================"
 echo "= Starting the TunnelServer on the outside of the firewall"
-echo "= TunnelServer -t 2345 -u 2345 1234"
+echo "= TunnelServer -t 5349 -u 5349 1234"
 echo "================================================================================"
+
+# Some useful ports:
+# 3478 STUN and TURN services, use to discover public addresses and relay media traffic
+# 5349 used to relay media for WebRTC in conjunction with STUN and TURN
 
 # tunnel server listens for tunnel connects on port 1234
 #                       for external TCP connections on port 2345
 #                       for external UDP packets on port 2345
-TunnelServer -t 2345 -u 2345 1234
+TunnelServer -t 5349 -u 5349 1234
 
--- test/live-udp-sender.sh
+++ test/live-udp-sender.sh
@@ -4,9 +4,9 @@ export PATH=/Users/griff/Install/tunnel/bin
 
 echo "================================================================================"
 echo "= Starting a UDP sender"
-echo "= Python_SendTest.py webrtc.mlab.no 2345 < DUMP.txt"
+echo "= Python_SendTest.py webrtc.mlab.no 5349 < DUMP.txt"
 echo "================================================================================"
 
 # iperf client sends UDP packets to port 2345 (outer tunnel UDP)
-python3 ./Python_SendTest.py webrtc.mlab.no 2345 < DUMP.txt
+python3 ./Python_SendTest.py webrtc.mlab.no 5349 < DUMP.txt
 
