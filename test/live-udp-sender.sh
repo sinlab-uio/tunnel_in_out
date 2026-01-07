ff903a6999acfaeec0efda95dac24917e5d04583
Carsten Griwodz <griff@simula.no>
2026-01-07 09:57:28 +0100

    live script for the Mac

---
+++ test/live-udp-sender.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH=/Users/griff/Install/tunnel/bin
+
+echo "================================================================================"
+echo "= Starting a UDP sender"
+echo "= Python_SendTest.py webrtc.mlab.no 2345 < DUMP.txt"
+echo "================================================================================"
+
+# iperf client sends UDP packets to port 2345 (outer tunnel UDP)
+python3 ./Python_SendTest.py webrtc.mlab.no 2345 < DUMP.txt
+
