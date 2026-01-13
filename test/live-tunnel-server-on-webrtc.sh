4eb10a6c6404de456f3e21337e77173794004a69
Carsten Griwodz <griff@ifi.uio.no>
2026-01-13 13:31:29 +0000

    setting to receive data from UPV

---
+++ test/live-tunnel-server-on-webrtc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export PATH=$PATH:/home/griff/Install/tunnel/bin
+
+echo "================================================================================"
+echo "= Starting the TunnelServer on the outside of the firewall"
+echo "= TunnelServer $* -t 5349 -u 5349 1234"
+echo "================================================================================"
+
+# Some useful ports:
+# 3478 STUN and TURN services, use to discover public addresses and relay media traffic
+# 5349 used to relay media for WebRTC in conjunction with STUN and TURN
+
+# tunnel server listens for tunnel connects on port 1234
+#                       for external TCP connections on port 2345
+#                       for external UDP packets on port 2345
+TunnelServer $* -t 9090 -u 12121 5201
+
