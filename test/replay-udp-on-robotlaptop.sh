cabcaf48a1c7f413f6b93798ead878cc1bb889f0
Carsten Griwodz <griff@simula.no>
2026-01-15 14:29:00 +0100

    a script for resend UDP hand commands

--- test/replay-udp-on-robotlaptop.sh
+++ test/replay-udp-on-robotlaptop.sh
@@ -1,2 +1,4 @@
-tcprewrite --infile=udponly.pcap --srcipmap=0.0.0.0/0:192.168.50.152/32 --dstipmap=0.0.0.0/0:192.168.50.152/32 -i enp3s0
+#!/bin/bash
+# tcprewrite -v --srcipmap=0.0.0.0/0:192.168.50.3/32 --dstipmap=0.0.0.0/0:192.168.50.152/32 -i udponly.pcap -o udponly-local.pcap
+tcpreplay -v udponly-local.pcap -i en0
 
