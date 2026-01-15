129e6e21f5fc56e5c2e1b715cc5560d98bdffcca
Carsten Griwodz <griff@simula.no>
2026-01-15 13:25:25 +0100

    tunnel client with parameters for actual tests

--- test/live-tunnel-client-to-webrtc.sh
+++ test/live-tunnel-client-to-webrtc.sh
@@ -8,5 +8,5 @@ echo "==========================================================================
 # tunnel client makes a tunnel to webrtc.mlab.no:3478
 #               forwards TCP connections from the tunnel to localhost 1235
 #               forwards UDP packets from the tunnel to localhost:1235
-$HOME/Install/tunnel/bin/TunnelClient $* -t localhost:1235 -u localhost:1235 webrtc.mlab.no:3478
+$HOME/Install/tunnel/bin/TunnelClient $* -t localhost:9090 -u localhost:12121 webrtc.mlab.no:5201
 
