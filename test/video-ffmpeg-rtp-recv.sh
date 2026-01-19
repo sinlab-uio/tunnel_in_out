f9c3057d57d137555e82be48b264606adc75a856
Carsten Griwodz <griff@simula.no>
2026-01-19 13:01:09 +0100

    video

--- test/video-ffmpeg-rtp-recv.sh
+++ test/video-ffmpeg-rtp-recv.sh
@@ -3,3 +3,9 @@
 # Play back from RTP
 ffplay -protocol_whitelist "file,rtp,udp" -i video.sdp
 
+# Google says
+# ffplay -fflags nobuffer -flags low_delay -probesize 32 -analyzeduration 1 -strict experimental -framedrop -f rtp rtp://[DESTINATION_IP]:[PORT]
+# which is weird
+
+
+
