4bccb4b92ab58a3ae56da843f001569ca1f384ca
Carsten Griwodz <griff@simula.no>
2026-01-19 12:52:17 +0100

    video sender

--- test/video-ffmpeg-rtp-send.sh
+++ test/video-ffmpeg-rtp-send.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-ffplay -f v4l2 -video_size 3840x1920 -input_format mjpeg -codec:v mjpeg -framerate 30 -fflags +nobuffer -i /dev/video0
+DEST=192.169.50.3
+
+echo "Sending video from the camera to destination ${DEST}"
+
+# ffplay -f v4l2 -video_size 3840x1920 -input_format mjpeg -codec:v mjpeg -framerate 30 -fflags +nobuffer -i /dev/video0
 
 # -re read input at native rate
 # -an disable audio
 # -c:v copy copy the audio format without change
 # -sdp_file <file> create the SDP file
 # ffmpeg -re -i input.mp4 -an -c:v copy -f rtp -sdp_file video.sdp "rtp://192.168.1.109:5004"
+
+ffmpeg -f v4l2 -video_size 3840x1920 -input_format mjpeg -codec:v mjpeg -framerate 30 \
+       -fflags +nobuffer \
+       -i /dev/video0 \
+       -an -c:v copy -f rtp -sdp_file video.sdp "${DEST}"
 
 # Play back from RTP
 # ffplay -protocol_whitelist "file,rtp,udp" -i video.sdp
