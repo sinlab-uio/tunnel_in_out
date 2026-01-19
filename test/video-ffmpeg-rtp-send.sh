befb2eed689f3a118a500cb22b4b2a5a75997b4a
Carsten Griwodz <griff@simula.no>
2026-01-19 15:53:54 +0100

    disable audio in ffmepg

--- test/video-ffmpeg-rtp-send.sh
+++ test/video-ffmpeg-rtp-send.sh
@@ -13,10 +13,11 @@ echo "Sending video from the camera to destination ${DEST}"
 # ffmpeg -re -i input.mp4 -an -c:v copy -f rtp -sdp_file video.sdp "rtp://192.168.1.109:5004"
 
 ffmpeg -use_wallclock_as_timestamps 1 \
-       -re \
        -f v4l2 -video_size 3840x1920 -input_format mjpeg -codec:v mjpeg -framerate 30 \
        -fflags +nobuffer \
        -i /dev/video0 \
+       -re \
+       -an \
        -c:v libx264 -preset ultrafast -tune zerolatency -fflags nobuffer -bf 0 -pix_fmt yuv420p \
        -x264opts keyint=30:min-keyint=1:scenecut=-1 \
        -f rtp -sdp_file video.sdp "rtp://${DEST}:5004"
