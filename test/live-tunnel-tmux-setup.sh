a8723a7c028a43eda44b54ba7a982ecee5624a8d
Carsten Griwodz <griff@simula.no>
2026-01-22 08:33:04 +0100

    easily restartable side-by-side tunnel using tmux

--- test/live-tunnel-tmux-setup.sh
+++ test/live-tunnel-tmux-setup.sh
@@ -10,7 +10,8 @@ tmux split-window -h -t $SESSION:0.0
 
 # TOP LEFT: 0.0
 # TunnelServer running on webrtc.mlab.no
-tmux send-keys -t 0.0 'ssh webrtc.mlab.no ~/Install/tunnel/bin/TunnelServer $* -t 9090 -u 12121 5201' C-M
+tmux send-keys -t 0.0 'ssh webrtc.mlab.no' C-M
+tmux send-keys -t 0.0 'Install/tunnel/bin/TunnelServer $* -t 9090 -u 12121 5201' C-M
 
 # TOP RIGHT: 0.1
 # TunnelCLient running locally
