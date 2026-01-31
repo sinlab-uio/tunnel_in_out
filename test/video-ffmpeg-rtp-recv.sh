42ee02f9811d163ed2dd7f955e78150e4d7700ad
Carsten Griwodz <griff@simula.no>
2026-01-31 11:59:55 +0100

    test uses SDP file with IP as param

--- test/video-ffmpeg-rtp-recv.sh
+++ test/video-ffmpeg-rtp-recv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+SELF=192.168.50.3
+
 # Play back from RTP
-ffplay -protocol_whitelist "file,rtp,udp" -i video.sdp
+ffplay -protocol_whitelist "file,rtp,udp" -i sdp-live-${SELF}.sdp
 
 # Google says
 # ffplay -fflags nobuffer -flags low_delay -probesize 32 -analyzeduration 1 -strict experimental -framedrop -f rtp rtp://[DESTINATION_IP]:[PORT]
